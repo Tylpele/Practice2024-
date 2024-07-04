d55d80bb0af6c16524d388e25e5014a2f55303b6
Danil <shashkoff.danil2016@yandex.ru>
2024-07-04 15:06:17 +0600

    change gitignore

---
+++ СhatBot/wwwroot/js/site.js
@@ -0,0 +1,42 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var connection = new signalR.HubConnectionBuilder()
+        .withUrl("/chatHub")
+        .build();
+
+    connection.on("ReceiveMessage", function (message) {
+        var li = document.createElement("li");
+        document.getElementById("messagesList").appendChild(li);
+        li.textContent = `Bot: ${message}`;
+    });
+
+    connection.start().then(function () {
+        console.log("SignalR Connected");
+
+        document.getElementById("sendButton").addEventListener("click", function (event) {
+            var message = document.getElementById("inputText").value;
+
+            if (!message) {
+                alert("Message cannot be empty");
+                return;
+            }
+
+            var connectionId = connection.connectionId; 
+
+            var formattedMessage = `${connectionId}:${message}`;
+
+            var li = document.createElement("li");
+            document.getElementById("messagesList").appendChild(li);
+            li.textContent = `User: ${message}`;
+
+            connection.invoke("SendMessageToQueue", formattedMessage).catch(function (err) {
+                return console.error(err.toString());
+            });
+
+            document.getElementById("inputText").value = "";
+
+            event.preventDefault();
+        });
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+});
