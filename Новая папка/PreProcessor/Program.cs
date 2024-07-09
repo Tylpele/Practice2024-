338f9ea4741332507c216b1b2e1e0cf45642ffa2
Danil <shashkoff.danil2016@yandex.ru>
2024-07-09 12:30:10 +0600

    fdsggfsg

---
+++ Новая папка/PreProcessor/Program.cs
@@ -0,0 +1,37 @@
+﻿//using MyPreProcessorApplication;
+using NLog;
+using PreProcessor1;
+
+class Program
+{
+    //private readonly string CurrentQueue = "pre-queue";
+    private readonly NLog.Logger _logger = LogManager.GetCurrentClassLogger();
+    static void Main(string[] args)
+    {
+        string CurrentQueue = "pre-queue";
+        RabbitMqService.StartListening(CurrentQueue, PreHandleMessage);
+        while (true)
+        { 
+            Thread.Sleep(10000);
+        }
+    }
+    private static void PreHandleMessage(string message)
+    {
+        string NextQueue = "queue";
+        var parts = message.Split(':');
+        if (parts.Length != 2)
+        {
+            //_logger.Error("Invalid message format: " + message);
+            return;
+        }
+
+        var ConnectionId = parts[0];
+        var messageContent = parts[1].ToLower();
+
+        var newMessage = $"{ConnectionId}:{messageContent}";
+
+        RabbitMqService.SendMessage(newMessage, NextQueue);
+        Console.WriteLine("Message was sent to queue");
+        //_logger.Info("Message " + message + " was sent to queue");
+    }
+}
---
+++ Новая папка/PreProcessor/RabbitMqService.cs
@@ -0,0 +1,95 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace PreProcessor1
+{
+    public class RabbitMqService
+    {
+        private static readonly ConnectionFactory _factory;
+        private static readonly IConnection _connection;
+        private static readonly IModel _channel;
+
+        static RabbitMqService()
+        {
+            _factory = new ConnectionFactory { HostName = "localhost" };
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public static void SendMessage(object obj, string queueName)
+        {
+            var message = JsonSerializer.Serialize(obj);
+            SendMessage(message, queueName);
+        }
+
+        public static void SendMessage(string inputText, string queueName)
+        {
+            _channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            var properties = _channel.CreateBasicProperties();
+            properties.Headers = new Dictionary<string, object>
+            {
+                { "Type", Encoding.UTF8.GetBytes("userMessage") } // Преобразование строки в byte[]
+            };
+
+            Console.WriteLine("Message Headers:");
+            foreach (var header in properties.Headers)
+            {
+                Console.WriteLine($"{header.Key}: {Encoding.UTF8.GetString((byte[])header.Value)}");
+            }
+
+            var message = Encoding.UTF8.GetBytes(inputText);
+            _channel.BasicPublish(exchange: "",
+                                  routingKey: queueName,
+                                  basicProperties: properties,
+                                  body: message);
+        }
+
+        public static void StartListening(string queueName, Action<string> onMessageReceived)
+        {
+            _channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                if (ea.BasicProperties.Headers != null &&
+                    ea.BasicProperties.Headers.TryGetValue("Type", out var typeHeader) &&
+                    typeHeader is byte[] headerBytes &&
+                    Encoding.UTF8.GetString(headerBytes) == "userMessage")
+                {
+                    _channel.BasicAck(ea.DeliveryTag, false);
+                    onMessageReceived(message);
+                }
+                else
+                {
+                    _channel.BasicNack(ea.DeliveryTag, false, true);
+                }
+            };
+
+            _channel.BasicConsume(
+                queue: queueName,
+                autoAck: false,
+                consumer: consumer
+            );
+        }
+    }
+}
